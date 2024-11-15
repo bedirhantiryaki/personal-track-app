3c180a66a8120bd55086b8d0de5c42b574ce7c0c
bedirhantiryaki <bedirhantiryaki00@gmail.com>
2024-11-15 10:25:49 +0300

    Add css file main page and update main page.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div>
       <h1>Personel Yönetimi</h1>
+
+      {/* Personel Arama Alanı */}
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Personel Ara"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button>Arama</button>
+      </div>
+
       <div>
         <Link to="/addEmployee">
           <button>Personel Ekle</button>
@@ -15,7 +31,7 @@ function App() {
       </div>
       <div>
         <button>Personel Listele</button>
-      </div>{" "}
+      </div>
     </div>
   );
 }
